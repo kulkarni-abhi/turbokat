73870bfb20fded49180bdc73037b70a3e9faea47
Abhishek Kulkarni <kulkarniabhi@vmware.com>
2019-04-24 19:14:33 +0530

    Coin Change Problem

---
+++ coin_change.py
@@ -0,0 +1,23 @@
+def get_change(amount, coins, change):
+    if sum(change) == amount:
+        yield change
+    elif sum(change) > amount:
+        pass
+    elif coins == []:
+        pass
+    else:
+        for c in get_change(amount, coins, change + [coins[0]]):
+            yield c
+        for c in get_change(amount, coins[1:], change):
+            yield c
+
+total = 100
+denominations = [25, 50]
+
+solutions = [s for s in get_change(total, denominations, [])]
+i = 1
+for s in solutions:
+    print str(i) + " : " + str(s)
+    i += 1
+
+print 'optimal solution:', min(solutions, key=len)
